91aa2c5cf87f04744ee8771eed849a3e7d98da7e
James S. Wang <JJamesWWang@gmail.com>
2022-02-05 22:28:48 -0500

    Reminder: Move to cog and add more detail to message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from bot import Bot
+from reminder import Reminder
+from nextcord.ext import commands
 from decouple import config
 
 
-bot = Bot(command_prefix="$")
+bot = commands.Bot(command_prefix="$")
+bot.add_cog(Reminder(bot))
 bot.run(config("TOKEN"))
--- reminder.py
+++ reminder.py
@@ -1,4 +1,40 @@
+from nextcord.ext import commands, tasks
 from datetime import datetime
+import pytz
+
+
+class Reminder(commands.Cog):
+    TEST_CHANNEL = 939658799059451904
+    ACTIVE_DEVS = "<@&938959783510294619>"
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.reminder.start()
+
+    @tasks.loop(seconds=30)
+    async def reminder(self):
+        channel = self.bot.get_channel(self.TEST_CHANNEL)
+        now = datetime.now(pytz.timezone("America/New_York"))
+        if is_30_minutes_before_check_in(now):
+            await channel.send(f"{self.ACTIVE_DEVS} Week.ly Check-in in 30 minutes!")
+        elif is_check_in_time(now):
+            await channel.send(f"{self.ACTIVE_DEVS} Week.ly Check-in now!")
+        elif is_30_minutes_before_hack_session(now):
+            message = await channel.send(
+                f"{self.ACTIVE_DEVS}> Week.ly Hack Session in 30 minutes! "
+                "Please react with ✅ if you can make it, "
+                "⌛ if you will be late, and ❌ if you can't make it."
+            )
+            await message.add_reaction("✅️")
+            await message.add_reaction("⌛")
+            await message.add_reaction("❌")
+        elif is_hack_session_time(now):
+            await channel.send("@everyone Week.ly Hack Session now!")
+        print(f"Looped: {now}")
+
+    @reminder.before_loop
+    async def before_reminder(self):
+        await self.bot.wait_until_ready()
 
 
 def is_30_minutes_before_check_in(time: datetime) -> bool:
