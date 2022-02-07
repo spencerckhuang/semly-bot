25ed0e48b8f733cb43344800e3d7c2d3fa757456
James S. Wang <JJamesWWang@gmail.com>
2022-02-07 10:38:48 -0500

    Reminder: Change reminders to wednesday 11am and saturday 11am

--- reminder.py
+++ reminder.py
@@ -62,16 +62,16 @@ class Reminder(commands.Cog):
 
 
 def is_30_minutes_before_check_in(time: datetime) -> bool:
-    return time.weekday() == 0 and time.hour == 10 and time.minute == 30
+    return time.weekday() == 2 and time.hour == 10 and time.minute == 30
 
 
 def is_check_in_time(time: datetime) -> bool:
-    return time.weekday() == 0 and time.hour == 11 and time.minute == 0
+    return time.weekday() == 2 and time.hour == 11 and time.minute == 0
 
 
 def is_30_minutes_before_hack_session(time: datetime) -> bool:
-    return time.weekday() == 3 and time.hour == 15 and time.minute == 30
+    return time.weekday() == 5 and time.hour == 10 and time.minute == 30
 
 
 def is_hack_session_time(time: datetime) -> bool:
-    return time.weekday() == 3 and time.hour == 16 and time.minute == 0
+    return time.weekday() == 5 and time.hour == 11 and time.minute == 0
