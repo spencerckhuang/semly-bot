623dd8106ef4cce9edcc2a03a4b0fadb644492fb
James S. Wang <JJamesWWang@gmail.com>
2022-03-12 01:24:29 -0500

    Execute: Add eval cog

---
+++ execute.py
@@ -0,0 +1,30 @@
+from nextcord import Message
+from nextcord.ext.commands import Bot, Cog, Context, command
+
+
+class ExecuteCog(Cog):
+    SUPERUSER = 229732075203330049
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @command()
+    async def execute(self, ctx: Context, *, message: str):
+        if ctx.message.author.id == self.SUPERUSER:
+            try:
+                result = eval(message)
+                await ctx.send(result)
+            except Exception as e:
+                await ctx.send(str(e))
+                raise e from e
+
+    @Cog.listener()
+    async def on_message_edit(self, before: Message, after: Message):
+        if after.content.startswith(self.bot.command_prefix + self.execute.name):
+            context = await self.bot.get_context(after)
+            await self.execute(
+                context,
+                message=after.content[
+                    len(self.bot.command_prefix + self.execute.name) + 1 :
+                ],
+            )
